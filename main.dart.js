e5cc9b448315ad904fb256594cb29129714a21df
Ibas2 <ibragim.020205@gmail.com>
2023-12-13 18:19:22 +0300

    dfsf

--- main.dart.js
+++ main.dart.js
@@ -25287,16 +25287,15 @@ q7:function q7(){},
 vx:function vx(){},
 Xj:function Xj(){},
 xt:function xt(a){this.a=a},
-QJ:function QJ(a,b,c,d,e,f,g,h){var _=this
-_.d=a
-_.e=b
-_.f=c
-_.r=d
-_.w=e
-_.x=f
-_.y=g
+QJ:function QJ(a,b,c,d,e,f,g){var _=this
+_.e=a
+_.f=b
+_.r=c
+_.w=d
+_.x=e
+_.y=f
 _.a=null
-_.b=h
+_.b=g
 _.c=null},
 agB:function agB(a){this.a=a},
 agz:function agz(a){this.a=a},
@@ -83922,15 +83921,14 @@ A.q7.prototype={}
 A.vx.prototype={}
 A.Xj.prototype={}
 A.xt.prototype={
-ae(){var s="dark",r=Telegram.WebApp.initData,q=J.c(Telegram.WebApp.colorScheme,s),p=A.aqO(Telegram.WebApp.backgroundColor),o=J.c(Telegram.WebApp.colorScheme,s)?A.aqO(Telegram.WebApp.backgroundColor):B.f2,n=J.c(Telegram.WebApp.colorScheme,s)?B.i:B.m,m=J.c(Telegram.WebApp.colorScheme,s)?B.Bw:B.i
-return new A.QJ(r,q,p,o,n,m,A.aG8(),B.j)}}
+ae(){var s="dark",r=J.c(Telegram.WebApp.colorScheme,s),q=A.aqO(Telegram.WebApp.backgroundColor),p=J.c(Telegram.WebApp.colorScheme,s)?A.aqO(Telegram.WebApp.backgroundColor):B.f2,o=J.c(Telegram.WebApp.colorScheme,s)?B.i:B.m,n=J.c(Telegram.WebApp.colorScheme,s)?B.Bw:B.i
+return new A.QJ(r,q,p,o,n,A.aG8(),B.j)}}
 A.QJ.prototype={
-aC(){var s=this
-J.FQ(Telegram.WebApp.BackButton,!1)
+aC(){J.FQ(Telegram.WebApp.BackButton,!1)
 J.FQ(Telegram.WebApp.MainButton,!1)
-if(!s.e)Telegram.WebApp.setHeaderColor("#F2F2F7")
-s.y.E(0,new A.pg(s.d))
-s.aT()},
+if(!this.e)Telegram.WebApp.setHeaderColor("#F2F2F7")
+this.y.E(0,new A.pg("query_id=AAHDeggyAAAAAMN6CDIgYEuC&user=%7B%22id%22%3A839416515%2C%22first_name%22%3A%22Ibragim%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22lbraqim%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1701339510&hash=183af7302191bd326ce8c17515f07cd07d29c5fa8b0b63630e873114046fa80a"))
+this.aT()},
 H(a){return A.zF(new A.w5(new A.agB(this),this.y,null,null,t._o))}}
 A.agB.prototype={
 $2(a,b){var s=null,r="public/images/pair.png"
